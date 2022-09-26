f090f437e413189ae652d8ef78629ccb1f136dae
Athallah Putra <atha.omniappsolutions@gmail.com>
2022-09-26 07:17:47 +0700

    Added Login page

---
+++ login.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <title>Halaman Login</title>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="login-style.css">
+    </head>
+
+    <body>
+        <!-- partial:index.partial.html -->
+        <div id="bg"></div>
+
+    <form>
+            <div class="form-field">
+                <input type="email" placeholder="Email / Username" required/>
+            </div>
+  
+            <div class="form-field">
+                <input type="password" placeholder="Password" required/>
+            </div>
+  
+            <div class="form-field">
+                <button class="btn" type="submit">Log in</button>
+            </div>
+    </form>
+
+<!-- partial -->
+    </body>
+</html>
