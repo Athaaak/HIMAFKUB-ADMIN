77125519946ea0c7a4d006720d52bac9186a2f8d
Athallah Putra <atha.omniappsolutions@gmail.com>
2022-10-21 13:03:50 +0700

    Added main website navbar

---
+++ admin-site/dashboard/main.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link href="../../node_modules/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+        <link rel="stylesheet" href="./main.css">
+    </head>
+
+    <body>
+        <!-- Navbar -->
+        <div class="page-bg">
+        <nav class="navbar navbar-expand-lg">
+            <div class="container">
+                <a class="navbar-brand" href="#">
+                        <img src="../../img/aksara.png" alt="AksaraAsa" class="logo">
+                        <label class="text-logo">BEM ASKARA ASA <br>FK UB 2022</label>
+                </a>
+                    <button class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarMenu">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarMenu">
+                        <ul class="navbar-nav">
+                            <li class="nav-item">
+                            <a class="nav-link" aria-current="page" href="#">Profil</a>
+                            </li>
+                            <li class="nav-item">
+                            <a class="nav-link" href="#">Lembaga & Himpunan</a>
+                            </li>
+                            <li class="nav-item">
+                            <a class="nav-link" href="#">E-SKK</a>
+                            </li>
+                            <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Kementrian & Biro
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="#">Action</a></li>
+                                <li><a class="dropdown-item" href="#">Another action</a></li>
+                                <li><a class="dropdown-item" href="#">Something else here</a></li>
+                            </ul>
+                            </li>
+                            <li class="nav-item">
+                            <a class="nav-link" href="#">SOP Kolegium</a>
+                            </li>
+                            <li class="nav-item">
+                            <a class="nav-link" href="#">Arsip</a>
+                            </li>
+                            <li class="nav-item">
+                            <a class="nav-link" href="#">Artikel</a>
+                            </li>
+                        </ul>
+                    </div>
+            </div> 
+        </nav>
+
+        <!-- Content -->
+
+        <div class="content-container">
+            <img src="../../img/aksara.png" class="content-logo">
+        </div>
+        
+        
+            <script src="../../node_modules/bootstrap/dist/js/bootstrap.bundle.js"></script>
+        </div>
+    </body>
+</html>
